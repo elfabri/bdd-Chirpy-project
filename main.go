95f0ddbee656415a04de615dbbf010e105cbd6e6
elfabri <navarro.fabricio.nahuel@gmail.com>
2025-02-03 13:27:07 -0300

    stateful handlers view count and routing

--- main.go
+++ main.go
@@ -3,7 +3,27 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	// "structs"
+	"sync/atomic"
 )
+
+// stateful handlers
+type apiConfig struct {
+    // atomic for safely increment
+    // and read an integer value
+    // across multiple goroutines (HTTP requests)
+	fileserverHits atomic.Int32
+}
+
+// middleware to count views
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        cfg.fileserverHits.Add(1)
+        next.ServeHTTP(w, r)
+    })
+}
 
 // readiness handler
 func readiness(resW http.ResponseWriter, req *http.Request) {
@@ -12,17 +32,35 @@ func readiness(resW http.ResponseWriter, req *http.Request) {
     resW.Write([]byte("OK"))
 }
 
+// view count handler
+func (cfg *apiConfig) views(resW http.ResponseWriter, _ *http.Request) {
+    resW.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+// view count reset
+func (cfg *apiConfig) reset(_ http.ResponseWriter, _ *http.Request) {
+    cfg.fileserverHits.Store(0)
+}
+
 func main() {
     mux := http.NewServeMux()
     server := &http.Server{
         Addr:       ":8080",
         Handler:    mux,
     }
+    apiCfg := apiConfig {fileserverHits: atomic.Int32{}}
+
+    // handler main page
+    handler := http.StripPrefix("/app/", http.FileServer(http.Dir(".")))
 
     // readiness endpoint
-    mux.HandleFunc("/healthz", readiness)
+    mux.HandleFunc("GET /healthz", readiness)
 
-    mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+    // view couter show & reset
+    mux.HandleFunc("GET /metrics", apiCfg.views)
+    mux.HandleFunc("POST /reset", apiCfg.reset)
+
+    mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
     mux.Handle("/assets", http.FileServer(http.Dir("./assets/logo.png")))
 
     if err := server.ListenAndServe(); err != nil {
